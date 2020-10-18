3d38410c6649877123d9b5e900e7f790561e9a6a
Sajal Sharma <sajal.4591@gmail.com>
2020-10-19 00:16:21 +0530

    setup repo

---
+++ app/worker/worker.py
@@ -0,0 +1,10 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.secrets')
+app = Celery()
+app.conf.CELERY_ROUTES = {}
+
+app.conf.update(settings.CELERY)
